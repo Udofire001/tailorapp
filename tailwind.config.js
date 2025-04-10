16df2389b5009caa1902d06834b04adfccfb4a5c
udofire001 <gilbert.a.udofia@gmail.com>
2025-04-10 11:54:06 +0100

    feat: progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html","./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
